03630f2adc5590fc3b7bc57e4f7e4b60d03ec58c
xlh <1847877071@qq.com>
2021-06-27 22:56:19 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import '@/styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建vue根实例，将router、store(vuex容器)配置到根实例中
+// 将APP根组件渲染到#app节点
 new Vue({
   router,
   store,
